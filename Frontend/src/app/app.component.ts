eeedaf53cf15c59796f0910fc2f7501be2ece47d
Jorik Spiesschaert <jorik.spiesschaert.y9481@student.hogent.be>
2018-03-16 00:32:25 +0100

    Toevoegen API Service aan Frontend

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private apiService: ApiService){}
+
+  ngOnInit(){
+    this.apiService.getMessages();
+  }
 }
